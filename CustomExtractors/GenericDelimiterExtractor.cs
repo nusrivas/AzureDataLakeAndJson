e2eddaeb2ff2d8f2a4c12476f3d7ca90cf6c424c
rashah <rashah@RASHAH-M01>
2017-02-08 17:48:01 -0800

    Added new extractors.

---
+++ CustomExtractors/GenericDelimiterExtractor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Analytics.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CustomExtractors
+{
+    public class GenericDelimiterExtractor : IExtractor
+    {
+        private char delimiter;
+
+        public GenericDelimiterExtractor(char delimiter)
+        {
+            this.delimiter = delimiter;
+        }
+
+        public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
+        {
+            using (StreamReader reader = new StreamReader(input.BaseStream))
+            {
+                string line = string.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split(this.delimiter);
+
+                    for (int i = 0; i < tokens.Length; i++)
+                    {
+                        output.Set(i, tokens[i]);
+                    }
+
+                    yield return output.AsReadOnly();
+                }
+            }
+        }
+    }
+}
---
+++ CustomExtractors/PipeExtractor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Analytics.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CustomExtractors
+{
+    /* input line: firstname|lastname|address|age
+    */
+
+    public class PipeExtractor : IExtractor
+    {
+        public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
+        {
+            using (StreamReader reader = new StreamReader(input.BaseStream))
+            {
+                string line = string.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split("|".ToCharArray());
+
+                    if (tokens.Length == 4)
+                    {
+                        output.Set<string>("FirstName", tokens[0]);
+                        output.Set<string>("LastName", tokens[1]);
+                        output.Set<string>("Address", tokens[2]);
+                        output.Set<int>("Age", int.Parse(tokens[3]));
+                    }
+
+                    yield return output.AsReadOnly();
+                }
+            }
+        }
+    }
+}
